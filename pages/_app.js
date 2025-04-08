b00f1f3168b0216467391afadaf452cd3542b131
Thaetami <tatanghaetami.97@gmail.com>
2025-04-08 15:25:00 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import "@/styles/globals.css";
+import PropTypes from 'prop-types';
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
+
+export default MyApp;
